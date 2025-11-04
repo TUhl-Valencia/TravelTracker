550af85266288c52be89f9827dc47656f580cfc4
TUhl-Valencia <tuhl@mail.valenciacollege.edu>
2025-11-03 21:49:37 -0500

    Update MainApp.java
    JavaDoc Additions

--- MainApp.java
+++ MainApp.java
@@ -1,6 +1,16 @@
 import javafx.application.Application;
 
+/**
+ * Entry point for the Travel Tracker application.
+ * Launches the JavaFX GUI.
+ */
 public class MainApp {
+
+    /**
+     * Main method to launch the TravelTrackerGUI.
+     *
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         Application.launch(TravelTrackerGUI.class, args);
     }
